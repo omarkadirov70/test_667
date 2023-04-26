0a0c15bdb81aaeb5ffcc57f222eb48c3acd102b9
Shyngyskhan <zhurabekov01int@gmail.com>
2023-04-26 22:30:10 +0600

    script btn added

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 document.querySelector('add')
+
+let ButtonVn = document.createElement('div')
+let input = document.createElement('input')
+let ButtonTch =  document.createElement('button')
+
+
+
+ButtonTch.classList.add('flex')
